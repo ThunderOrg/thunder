c69fd206891790ec8006fb5ea5f6a3b082ecf0ba
paranoidgabe <gloewen@crimson.ua.edu>
2013-05-10 14:37:56 -0500

    added events module

--- NetEvent.py
+++ NetEvent.py
@@ -5,7 +5,7 @@
 import socket, socketserver, threading, auth, time
 from dictionary import *
 from datetime import datetime
-from events import *
+import events as builtinEvents
 
 # Mapping from event name to function
 events = Dictionary()
@@ -35,8 +35,8 @@ class NetEvent():
       self.port = self.server.server_address[1]
       self.server.isDaemon = True
       self.start()
-      self.registerEvent("UTILIZATION", utilization)
-      self.registerEvent("SYSINFO", sysInfo)
+      self.registerEvent("UTILIZATION", builtinEvents.utilization)
+      self.registerEvent("SYSINFO", builtinEvents.sysInfo)
 
    # Start thread for handling requests
    def start(self):
