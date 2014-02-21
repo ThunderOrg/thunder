542cc744ab95983601a2a8414fc53112c4359bb9
paranoidgabe <gloewen@crimson.ua.edu>
2014-02-21 16:06:12 -0600

    Added web backup

---
+++ backupweb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DATE=`date +%Y-%m-%d`
+FNAME=web/$DATE
+EXT=tgz
+
+if [[ -e $FNAME.$EXT ]]; then
+    i=1
+    NEWFNAME=$FNAME-$i
+    while [[ -e $NEWFNAME.$EXT ]]; do
+       i=$(($i + 1))
+       NEWFNAME=$FNAME-$i 
+    done
+    FNAME=$NEWFNAME
+fi
+
+tar -czf $FNAME.$EXT -C / var/www
