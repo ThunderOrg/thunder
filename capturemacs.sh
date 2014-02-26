94feea1dd91edc48d814a8fe5f2345ada9dd05c1
thunder <thunder@thunderAdmin.(none)>
2014-02-25 21:08:25 -0600

    moved capturemacs.sh to netevent

--- capturemacs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run this to get a listing of mac addresses from TCPdump
-timeout 15 tcpdump -i eth0 -qtel broadcast and port bootpc > tmp
-awk '{if (length($1) == 17) print $1}' tmp | sort | uniq > mac.list
-rm tmp
