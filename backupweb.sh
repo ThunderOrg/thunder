6910650af700690c491d12b1f310b15dfc7bd104
thunder <thunder@thunderAdmin.(none)>
2014-02-25 21:07:47 -0600

    fixed backup script

--- backupweb.sh
+++ backupweb.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+WWW_ROOT=/var/www
+WD=$(pwd)/web
 DATE=`date +%Y-%m-%d`
-FNAME=web/$DATE
+FNAME=$DATE
 EXT=tgz
 
-if [[ -e $FNAME.$EXT ]]; then
+echo 'Backing up web...';
+
+if [[ -e $WD/$FNAME.$EXT ]]; then
     i=1
     NEWFNAME=$FNAME-$i
-    while [[ -e $NEWFNAME.$EXT ]]; do
+    while [[ -e $WD/$NEWFNAME.$EXT ]]; do
        i=$(($i + 1))
        NEWFNAME=$FNAME-$i 
     done
     FNAME=$NEWFNAME
 fi
 
-tar -czf $FNAME.$EXT -C / var/www
+cd $WWW_ROOT
+tar -czf $FNAME.$EXT ./*
+cd $WD
+
+mv $WWW_ROOT/$FNAME.$EXT $WD/$FNAME.$EXT
 git add $FNAME.$EXT
+echo 'Done.';
