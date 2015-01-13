f8ad83c9d5ae70b30d377cbc4ba67c9f740c3b23
Gabriel <gloewen@crimson.ua.edu>
2015-01-12 18:53:05 -0600

    update

---
+++ ThunderRPC/getImage.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+URL=$1
+IMAGEDIR=$2
+{
+if [[ -d tmp ]]
+then
+   rm -rf tmp
+fi
+mkdir tmp
+cd tmp
+wget -Ufirefox $URL -O image.tar
+tar xvf image.tar
+rm image.tar
+for f in *.tar.@(xz|gz); do
+   mv $f $IMAGEDIR
+done
+} > /dev/null 2>&1
+
+for f in *.yml; do
+   parse_yaml $f
+done
--- ThunderRPC/storagenode.py
+++ ThunderRPC/storagenode.py
@@ -11,10 +11,13 @@ from thunder import *
 def importImage(*params):
    args = params[1]
    url = args[0]
+   imageDir = constants.get("default.imagedir")
    # Download the image from the url
-   p = subprocess.Popen('wget -U firefox ' + url, shell=True) 
-   print(p.wait())
+   p = subprocess.Popen('getImage.sh ' + url + imageDir, stdout=subprocess.PIPE)
+   out,err=p.communicate()
+   print(out)
    # Untar the image
+   
    # Put the data from the YAML into the database
    # Store the image tar in the share
    return 'success'
