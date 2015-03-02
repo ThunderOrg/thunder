f39b7cf7b8699a4eb50a4b403b1edb3ee76fc7b1
Gabriel <gloewen@crimson.ua.edu>
2015-03-02 15:20:11 -0600

    test

--- ThunderRPC/destroyAll.sh
+++ ThunderRPC/destroyAll.sh
@@ -5,6 +5,10 @@ domains=`virsh pool-list --all | awk '{print $1}' | sed '1,2d' | sed '$d'`
 for domain in $domains; do
    virsh destroy $domain
    virsh undefine $domain
+   vols=`virsh vol-list $domain | awk '{print $1}' | sed '1,2d' | sed '$d'`
+   for vol in $vols; do
+      virsh vol-delete $vol --pool $domain
+   done
    virsh pool-destroy $domain
    virsh pool-delete $domain
    virsh pool-undefine $domain
--- ThunderRPC/destroyVM.sh
+++ ThunderRPC/destroyVM.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 virsh destroy $1
 virsh undefine $1
+vols=`virsh vol-list $1 | awk '{print $1}' | sed '1,2d' | sed '$d'`
+for vol in $vols; do
+      virsh vol-delete $vol --pool $domain
+done
 virsh pool-destroy $1
 virsh pool-delete $1
 virsh pool-undefine $1
